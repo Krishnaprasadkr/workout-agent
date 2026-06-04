112113e5ff5e3c5b61dbecb43f8ef93dcd5473ce
krishna17 <91798325+Krishnaprasadkr@users.noreply.github.com>
2026-06-04 17:32:28 +0530

    Update telegram_helper.py

--- telegram_helper.py
+++ telegram_helper.py
@@ -1,18 +1,17 @@
-import os
-import requests
-
-BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
-
-TELEGRAM_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-
 def send(text: str):
-    """Send a Markdown-formatted message to your Telegram chat."""
+    """Send Telegram message."""
+
     payload = {
         "chat_id": CHAT_ID,
         "text": text,
-        "parse_mode": "HTML",
     }
+
     resp = requests.post(TELEGRAM_URL, json=payload, timeout=15)
+
+    print("TELEGRAM RESPONSE:")
+    print(resp.status_code)
+    print(resp.text)
+
     resp.raise_for_status()
+
     print("Telegram message sent.")
